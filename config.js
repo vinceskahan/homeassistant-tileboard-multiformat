2d7b40af76b960b724e9147c651c3e3dab734ed8
Vince Skahan <vinceskahan@gmail.com>
2020-03-21 19:30:34 -0700

    nuff for today

--- config.js
+++ config.js
@@ -1,11 +1,3 @@
-/*
- This is an example configuration file.
-
- COPY OR RENAME THIS FILE TO config.js.
-
- Make sure you use real IDs from your HA entities.
-*/
-
 
 var CONFIG = {
    customTheme: null, // CUSTOM_THEMES.TRANSPARENT, CUSTOM_THEMES.MATERIAL, CUSTOM_THEMES.MOBILE, CUSTOM_THEMES.COMPACT, CUSTOM_THEMES.HOMEKIT, CUSTOM_THEMES.WINPHONE, CUSTOM_THEMES.WIN95
@@ -14,12 +6,8 @@ var CONFIG = {
    tileSize: 150,
    tileMargin: 6,
    serverUrl: 'http://' + location.hostname + ':8123',
-   //serverUrl: 'http://nuc2' + ':8123',
    wsUrl: 'ws://' + location.hostname + ':8123/api/websocket',
-   //wsUrl: 'ws://nuc2' + ':8123/api/websocket',
    authToken: null, // optional long-lived token (CAUTION: only if TileBoard is not exposed to the internet)
-   //googleApiKey: "XXXXXXXXXX", // Required if you are using Google Maps for device tracker
-   //mapboxToken: "XXXXXXXXXX", // Required if you are using Mapbox for device tracker
    debug: false, // Prints entities and state change info to the console.
    pingConnection: true, //ping connection to prevent silent disconnections
 
@@ -72,107 +60,155 @@ var CONFIG = {
          bg: 'images/bg1.jpeg',
          icon: 'mdi-home-outline', // home icon
          groups: [
+
             {
                title: 'Lights',
                width: 2,
                height: 3,
                items: [
 
-// ---------------------------------------------
+                  {
+                     position: [0, 0],
+                     title: 'Lightstrip',
+                     subtitle: 'Family Room',
+                     id: 'light.hue_lightstrip',
+                     type: TYPES.LIGHT,
+                     states: { on: "On", off: "Off" },
+                     icons: { on: "mdi-lightbulb-on", off: "mdi-lightbulb", },
+                     colorpicker: false
+                  },
+                  
+                  {
+                     position: [1, 0],
+                     title: 'Desk',
+                     subtitle: 'Office',
+                     id: 'light.desk',
+                     type: TYPES.LIGHT,
+                     states: { on: "On", off: "Off" },
+                     icons: { on: "mdi-lightbulb-on", off: "mdi-lightbulb", },
+                     colorpicker: false
+                  },
+                  
+                  {
+                     position: [0, 1],
+                     title: 'Overhead',
+                     subtitle: 'Baseball Room',
+                     id: 'light.overhead',
+                     type: TYPES.LIGHT,
+                     states: { on: "On", off: "Off" },
+                     icons: { on: "mdi-lightbulb-on", off: "mdi-lightbulb", },
+                     colorpicker: false
+                  },
+                  
+                  {
+                     position: [1, 1],
+                     title: 'Corner',
+                     subtitle: 'Master Bedroom',
+                     id: 'light.corner_lamp',
+                     type: TYPES.LIGHT,
+                     states: { on: "On", off: "Off" },
+                     icons: { on: "mdi-lightbulb-on", off: "mdi-lightbulb", },
+                     colorpicker: false
+                  },
 
-{
-   position: [0, 0],
-   title: 'Lightstrip',
-   subtitle: 'Family Room',
-   id: 'light.hue_lightstrip',
-   type: TYPES.LIGHT,
-   states: { on: "On", off: "Off" },
-   icons: { on: "mdi-lightbulb-on", off: "mdi-lightbulb", },
-   colorpicker: false
-},
-
-{
-   position: [1, 0],
-   title: 'Desk',
-   subtitle: 'Office',
-   id: 'light.desk',
-   type: TYPES.LIGHT,
-   states: { on: "On", off: "Off" },
-   icons: { on: "mdi-lightbulb-on", off: "mdi-lightbulb", },
-   colorpicker: false
-},
-
-{
-   position: [0, 1],
-   title: 'Overhead',
-   subtitle: 'Baseball Room',
-   id: 'light.overhead',
-   type: TYPES.LIGHT,
-   states: { on: "On", off: "Off" },
-   icons: { on: "mdi-lightbulb-on", off: "mdi-lightbulb", },
-   colorpicker: false
-},
-
-{
-   position: [1, 1],
-   title: 'Corner',
-   subtitle: 'Master Bedroom',
-   id: 'light.corner_lamp',
-   type: TYPES.LIGHT,
-   states: { on: "On", off: "Off" },
-   icons: { on: "mdi-lightbulb-on", off: "mdi-lightbulb", },
-   colorpicker: false
-},
-
-{
-   position: [0, 2],
-   type: TYPES.SENSOR_ICON,
-   title: 'Slider',
-   id: 'binary_sensor.ecolink_door_window_sensor_sensor',
-   states: {
-      on: "OPEN",
-      off: "closed",
-   },
-   icons: {
-      on: 'mdi-bell',
-      off: 'mdi-bell-off',
-   },
-},
-
-{
-   position: [1, 2],
-   width: 1,
-   height: 1,
-   title: 'Slider Sensor Battery',
-   subtitle: '',
-   type: TYPES.GAUGE,
-   id: 'sensor.ecolink_door_window_sensor_battery_level',
-   value: function(item, entity){
-      return entity.state;
-   },
-   settings: {
-      size: 100, // Defaults to 50% of either height or width, whichever is smaller
-      type: 'arch', // Options are: 'full', 'semi', and 'arch'. Defaults to 'full'
-      min: 0, // Defaults to 0
-      max: 100, // Defaults to 100
-      cap: 'round', // Options are: 'round', 'butt'. Defaults to 'butt'
-      thick: 6, // Defaults to 6
-      //label: 'Door Sensor Battery', // Defaults to undefined
-      append: '@attributes.unit_of_measurement', // Defaults to undefined
-      //prepend: '%', // Defaults to undefined
-      duration: 1500, // Defaults to 1500ms
-      thresholds: { 0: { color: 'black'}, },  // Defaults to undefined
-      labelOnly: false, // Defaults to false
-      foregroundColor: 'rgba(0, 150, 136, 1)', // Defaults to rgba(0, 150, 136, 1)
-      backgroundColor: 'rgba(0, 0, 0, 0.1)', // Defaults to rgba(0, 0, 0, 0.1)
-      fractionSize: 0, // Number of decimal places to round the number to. Defaults to current locale formatting
-   },
-},
-
-
-// ---------------------------------------------
-                ]        // end of items for Lights
+                ]        // end of items for Lights group
             },           // end of Lights group on Main page
+
+//---
+            {
+               title: 'Doors and Windows',
+               width: 2,
+               height: 3,
+               items: [
+                  
+                  {
+                     position: [0, 0],
+                     type: TYPES.SENSOR_ICON,
+                     title: 'Kitchen Slider',
+                     id: 'binary_sensor.ecolink_door_window_sensor_sensor',
+                     states: {
+                        on: "OPEN",
+                        off: "closed",
+                     },
+                     icons: {
+                        on: 'mdi-bell',
+                        off: 'mdi-bell-off',
+                     },
+                  },
+
+                  {
+                     position: [1, 0],
+                     width: 1,
+                     height: 1,
+                     title: 'Slider Sensor Battery',
+                     subtitle: '',
+                     type: TYPES.GAUGE,
+                     id: 'sensor.ecolink_door_window_sensor_battery_level',
+                     value: function(item, entity){
+                        return entity.state;
+                     },
+                     settings: {
+                        size: 100, // Defaults to 50% of either height or width, whichever is smaller
+                        type: 'arch', // Options are: 'full', 'semi', and 'arch'. Defaults to 'full'
+                        min: 0, // Defaults to 0
+                        max: 100, // Defaults to 100
+                        cap: 'round', // Options are: 'round', 'butt'. Defaults to 'butt'
+                        thick: 6, // Defaults to 6
+                        //label: 'Door Sensor Battery', // Defaults to undefined
+                        append: '@attributes.unit_of_measurement', // Defaults to undefined
+                        //prepend: '%', // Defaults to undefined
+                        duration: 1500, // Defaults to 1500ms
+                        thresholds: { 0: { color: 'black'}, },  // Defaults to undefined
+                        labelOnly: false, // Defaults to false
+                        foregroundColor: 'rgba(0, 150, 136, 1)', // Defaults to rgba(0, 150, 136, 1)
+                        backgroundColor: 'rgba(0, 0, 0, 0.1)', // Defaults to rgba(0, 0, 0, 0.1)
+                        fractionSize: 0, // Number of decimal places to round the number to. Defaults to current locale formatting
+                     },
+                  },
+
+                ]        // end of items for Doors and Windows
+            },           // end of Doors and Windows on Main page
+//---
+            {
+               title: 'Weather',
+               width: 2,
+               height: 3,
+               items: [
+                  
+                  {
+                     position: [0, 0],
+                     width: 1,
+                     height: 1,
+                     title: 'Outside Temp',
+                     subtitle: '',
+                     type: TYPES.GAUGE,
+                     id: 'sensor.outtemp',
+                     value: function(item, entity){
+                        return entity.state;
+                     },
+                     settings: {
+                        size: 100, // Defaults to 50% of either height or width, whichever is smaller
+                        type: 'arch', // Options are: 'full', 'semi', and 'arch'. Defaults to 'full'
+                        min: 0, // Defaults to 0
+                        max: 100, // Defaults to 100
+                        cap: 'round', // Options are: 'round', 'butt'. Defaults to 'butt'
+                        thick: 6, // Defaults to 6
+                        //label: 'Door Sensor Battery', // Defaults to undefined
+                        append: '@attributes.unit_of_measurement', // Defaults to undefined
+                        //prepend: '%', // Defaults to undefined
+                        duration: 1500, // Defaults to 1500ms
+                        thresholds: { 0: { color: 'black'}, },  // Defaults to undefined
+                        labelOnly: false, // Defaults to false
+                        foregroundColor: 'rgba(0, 150, 136, 1)', // Defaults to rgba(0, 150, 136, 1)
+                        backgroundColor: 'rgba(0, 0, 0, 0.1)', // Defaults to rgba(0, 0, 0, 0.1)
+                        fractionSize: 0, // Number of decimal places to round the number to. Defaults to current locale formatting
+                     },
+                  },
+
+                ]        // end of items for Doors and Windows
+            },           // end of Doors and Windows on Main page
+//-----
+//-----
 
          ]               // end of groups on Main page
       }                  // end of Main Page
